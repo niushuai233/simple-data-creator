1116cda2909faaae2d5967ec64c36315221644c3
niushuai233 <niushuai951101@gmail.com>
2024-08-14 11:53:27 +0800

    feat: :rocket: 初始化提交

---
+++ src/main/java/cc/niushuai/datacreator/DataCreatorApplication.java
@@ -0,0 +1,13 @@
+package cc.niushuai.datacreator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCreatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCreatorApplication.class, args);
+    }
+
+}
