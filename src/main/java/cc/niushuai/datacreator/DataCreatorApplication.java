ca222ab9178237b6e2f1b58eb17c412b7b25cde7
niushuai233 <niushuai951101@gmail.com>
2024-09-04 14:22:02 +0800

    feat: :musical_score:  扫描mapper路径

--- src/main/java/cc/niushuai/datacreator/DataCreatorApplication.java
+++ src/main/java/cc/niushuai/datacreator/DataCreatorApplication.java
@@ -1,13 +1,14 @@
 package cc.niushuai.datacreator;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("cc.niushuai.datacreator.biz.**.mapper")
 @SpringBootApplication
 public class DataCreatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DataCreatorApplication.class, args);
     }
-
 }
